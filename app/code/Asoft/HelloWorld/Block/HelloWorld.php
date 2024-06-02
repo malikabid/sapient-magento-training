069a6d0b8eb773c0709f3e9348c5771235489aa0
Abid H. Malik <malikabid2006@gmail.com>
2024-06-02 15:30:21 +0530

    How to create module and add block to store frontend

---
+++ app/code/Asoft/HelloWorld/Block/HelloWorld.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Asoft\HelloWorld\Block;
+
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    public function getMessage()
+    {
+        return "Hello World from the Block";
+    }
+}
