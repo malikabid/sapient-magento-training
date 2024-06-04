4bc2e88c7a45a04968f24e86f57f13bc1b1c1b92
Abid H. Malik <malikabid2006@gmail.com>
2024-06-04 10:14:44 +0530

    BlogGrpahql - V2

--- app/code/Asoft/BlogGraphql/Model/Resolver/Posts.php
+++ app/code/Asoft/BlogGraphql/Model/Resolver/Posts.php
@@ -2,40 +2,62 @@
 
 namespace Asoft\BlogGraphql\Model\Resolver;
 
-use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\SortOrderBuilder;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Asoft\Blog\Model\ResourceModel\Post\CollectionFactory as PostCollectionFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Asoft\Blog\Api\PostRepositoryInterface;
 
 class Posts implements ResolverInterface
 {
 
-    protected $_postCollectionFactory;
+    private $searchCriteriaBuilder;
+    private $filterBuilder;
+    private $sortOrderBuilder;
+    private $postRepository;
+
+
     public function __construct(
-        PostCollectionFactory $postCollectionFactory
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder,
+        SortOrderBuilder $sortOrderBuilder,
+        PostRepositoryInterface $postRepository
     ) {
-        $this->_postCollectionFactory = $postCollectionFactory;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
+        $this->sortOrderBuilder = $sortOrderBuilder;
+        $this->postRepository = $postRepository;
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, ?array $value = null, ?array $args = null)
     {
 
-        $postCollection = $this->_postCollectionFactory->create();
-
         if (isset($args['filter']['id'])) {
-            foreach ($args['filter']['id'] as $condition => $value) {
-                $postCollection->addFieldToFilter('entity_id', [$condition => $value]);
+            $idFilter = $args['filter']['id'];
+            foreach ($idFilter as $condition => $value) {
+                $filter = $this->filterBuilder
+                    ->setField('entity_id')
+                    ->setConditionType($condition)
+                    ->setValue($value)
+                    ->create();
+                $this->searchCriteriaBuilder->addFilters([$filter]);
             }
         }
+
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+        $postCollection = $this->postRepository->getList($searchCriteria)->getItems();
 
         $posts = [];
 
         foreach ($postCollection as $post) {
             $posts[] = [
-                'post_id' => $post->getId(),
+                'post_id' => $post->getPostId(),
                 'name' => $post->getName(),
-                'content' => $post->getContent(),
-                'short_content' => $post->getShortDescription()
+                'content' => $post->getContent()
             ];
         }
 
